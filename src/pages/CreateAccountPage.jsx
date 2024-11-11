43a674c263dbc086f95a9e9dc8438a08f958d9fd
Mark Abdallah <abdallah.marco@gmail.com>
2024-11-11 16:41:21 +0000

    update createAccountPage form look and fee;

--- src/pages/CreateAccountPage.jsx
+++ src/pages/CreateAccountPage.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+import { Button, Label, TextInput } from 'flowbite-react'
 
 const CreateAccountPage = () => {
   // State for form inputs
@@ -29,46 +30,64 @@ const CreateAccountPage = () => {
   return (
     <div className="flex-1 w-1/3 mx-auto justify-middle">
       <div className="flex flex-col space-y-4">
-        <h1 className="text-center">Create Account</h1>
+        <h1 className="text-center text-xl">Create Account</h1>
         {error && <p className="text-red-500 text-center">{error}</p>}
         {success && <p className="text-green-500 text-center">{success}</p>}
         <form className="flex flex-col border-2 border-gray-300 rounded-lg p-4 space-y-2" onSubmit={handleSubmit}>
-          <input
-            className="text-center"
-            type="text"
-            value={username}
-            onChange={(e) => {
-              setUsername(e.target.value)
-              setError('')
-            }}
-            placeholder="Enter username"
-          />
-          <input
-            className="text-center"
-            type="email"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value)
-              setError('')
-            }}
-            placeholder="Enter email"
-          />
-          <input
-            className="text-center"
-            type="password"
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value)
-              setError('')
-            }}
-            placeholder="Enter password"
-          />
-          <button
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="username" value="Username" />
+            </div>
+            <TextInput
+              id="username"
+              type="text"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value)
+                setError('')
+              }}
+              placeholder="Enter username"
+              required
+            />
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="email" value="Email" />
+            </div>
+            <TextInput
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value)
+                setError('')
+              }}
+              placeholder="Enter email"
+              required
+            />
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="password" value="Password" />
+            </div>
+            <TextInput
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value)
+                setError('')
+              }}
+              placeholder="Enter password"
+              required
+            />
+          </div>
+          <Button
             type="submit"
             className="px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Create Account
-          </button>
+          </Button>
         </form>
       </div>
     </div>
