d5ead2f4614ff158c2ea55d26efacaf5034b6c89
Mark Abdallah <abdallah.marco@gmail.com>
2024-11-11 16:02:11 +0000

    update createlistpage and optimized vite with more ignored directories and files

--- src/pages/CreateListPage.jsx
+++ src/pages/CreateListPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { addListToDB } from '../utils/indexedDB'
+import { Button, Label, TextInput } from 'flowbite-react'
 
 const CreateListPage = () => {
   const [listName, setListName] = useState('')
@@ -54,55 +55,74 @@ const CreateListPage = () => {
   return (
     <div className="flex-1 w-1/3 mx-auto justify-middle">
       <div className="flex flex-col space-y-4">
-        <h1 className="text-center">Create List</h1>
+        <h1 className="text-center text-xl">Create List</h1>
         {error && <p className="text-red-500 text-center">{error}</p>}
         <form className="flex flex-col border-2 border-gray-300 rounded-lg p-4 space-y-2" onSubmit={handleSubmit}>
-          <input
-            className="text-center"
-            type="text"
-            value={listName}
-            onChange={(e) => {
-              setListName(e.target.value)
-              setError('')
-            }}
-            placeholder="Enter list name"
-          />
-          <input
-            className="text-center"
-            type="text"
-            value={item}
-            onChange={(e) => setItem(e.target.value)}
-            placeholder="Enter item"
-          />
-          <ul className="text-center bg-sky-300 border-2 rounded-lg">
-            {items.length === 0 ? 'You have no items yet!' : items.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))}
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="listName" value="List Name" />
+            </div>
+            <TextInput
+              id="listName"
+              type="text"
+              value={listName}
+              onChange={(e) => {
+                setListName(e.target.value)
+                setError('')
+              }}
+              placeholder="Enter list name"
+              required
+            />
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="item" value="Item" />
+            </div>
+            <TextInput
+              id="item"
+              type="text"
+              value={item}
+              onChange={(e) => setItem(e.target.value)}
+              placeholder="Enter item"
+            />
+          </div>
+          <ul className="text-center bg-gray-100 border-2 border-gray-300 rounded-lg p-4 my-4">
+            {items.length === 0 ? (
+              <li className="text-gray-500">You have no items yet!</li>
+            ) : (
+              items.map((item, index) => (
+                <li key={index} className="text-gray-700">
+                  {item}
+                </li>
+              ))
+            )}
           </ul>
           <div className="flex flex-row justify-between">
-            <button
+            <Button
+              color="light"
               disabled={!item.trim()}
               onClick={handleAddItem}
               type="button"
-              className="px-3 py-2 w-1/3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="w-1/3"
             >
               Add Item
-            </button>
-            <button
+            </Button>
+            <Button
+              color="success"
               type="submit"
-              className="px-3 py-2 w-1/3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="w-1/3"
             >
               Create List
-            </button>
+            </Button>
           </div>
         </form>
-        <button
+        <Button
           onClick={handleCancelItem}
           type="button"
           className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
         >
           Cancel
-        </button>
+        </Button>
       </div>
     </div>
   )
